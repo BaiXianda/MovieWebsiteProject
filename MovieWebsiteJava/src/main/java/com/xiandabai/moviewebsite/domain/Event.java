c9fa6e4ec674de6d8a7636e2c781b0dc497deb70
Bobo <HKJBobo@gmail.com>
2020-03-24 20:32:06 -0400

    continuing add event domain

--- MovieWebsiteJava/src/main/java/com/xiandabai/moviewebsite/domain/Event.java
+++ MovieWebsiteJava/src/main/java/com/xiandabai/moviewebsite/domain/Event.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
+import java.util.Date;
 
 @Entity
 public class Event {
@@ -12,7 +13,62 @@ public class Event {
     @GeneratedValue
     private Long id;
 
+    @NotBlank(message = "must have a vote")
+    private Vote vote;
 
+    @NotBlank(message = "must set eventTime")
+    private Date eventTime;
 
+    @NotBlank(message = "must choose a movieList")
+    private MovieList movieList;
 
+    @NotBlank(message = "must set a title")
+    private String title;
+
+    public Event(@NotBlank(message = "must have a vote") Vote vote, @NotBlank(message = "must set eventTime") Date eventTime, @NotBlank(message = "must choose a movieList") MovieList movieList, @NotBlank(message = "must set a title") String title) {
+        this.vote = vote;
+        this.eventTime = eventTime;
+        this.movieList = movieList;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Vote getVote() {
+        return vote;
+    }
+
+    public void setVote(Vote vote) {
+        this.vote = vote;
+    }
+
+    public Date getEventTime() {
+        return eventTime;
+    }
+
+    public void setEventTime(Date eventTime) {
+        this.eventTime = eventTime;
+    }
+
+    public MovieList getMovieList() {
+        return movieList;
+    }
+
+    public void setMovieList(MovieList movieList) {
+        this.movieList = movieList;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
