8fd78e5b76b3c8e8746409991145e49771d6e85b
Xianda Bai <xiandabai@gwu.edu>
2020-03-24 20:07:06 -0400

    create store

--- movie-website/src/App.js
+++ movie-website/src/App.js
@@ -6,16 +6,19 @@ import Header from "./components/Layout/Header";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import AddGroup from "./components/Group/AddGroup";
+import store from "./store";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/addGroup" component={AddGroup} />
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addGroup" component={AddGroup} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
